2e3ffe241a6f3f92d032bbb5f3818c86a808aec6
zimgalia <zimgalia@gmail.com>
2019-12-17 11:32:52 +0200

    Generalize MappingAlgorithmBuilder

--- src/NodeMappingAlgorithms/MappingAlgorithmBuilder.java
+++ src/NodeMappingAlgorithms/MappingAlgorithmBuilder.java
@@ -2,15 +2,39 @@ package NodeMappingAlgorithms;
 
 import structures.Node;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.BiFunction;
 
-public class MappingAlgorithmBuilder {
-    public static NodeMappingAlgorithm build(String string, Node node, int treeDeletionLimit,
-                                                      int stringDeletionLimit,
-                                                      BiFunction<String,Character,Double>
-                                                     substitutionFunction) {
+class MappingAlgorithmBuilder {
+    static NodeMappingAlgorithm build(String string, Node node, int treeDeletionLimit,
+                                      int stringDeletionLimit,
+                                      BiFunction<String, Character, Double>
+                                              substitutionFunction) {
+        NodeMappingAlgorithm algorithm;
+        try {
+            Constructor constructor;
+            Class<?> mappingAlgorithmClass = node.getType().getMappingAlgorithm();
+            constructor = mappingAlgorithmClass
+                    .getConstructor(String.class, Node.class, int.class, int.class, BiFunction.class);
+            Object newInstance = constructor.newInstance(string, node, treeDeletionLimit,
+                    stringDeletionLimit, substitutionFunction);
+            if(mappingAlgorithmClass.isInstance(newInstance))
+                algorithm = ((NodeMappingAlgorithm) newInstance);
+            else
+                algorithm = getNodeMappingAlgorithm(string, node, treeDeletionLimit,
+                        stringDeletionLimit, substitutionFunction);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
+                | InvocationTargetException e) {
+            e.printStackTrace();
+            algorithm = getNodeMappingAlgorithm(string, node, treeDeletionLimit,
+                    stringDeletionLimit, substitutionFunction);
+        }
+        return algorithm;
+    }
+
+    private static NodeMappingAlgorithm getNodeMappingAlgorithm(String string, Node node, int treeDeletionLimit, int stringDeletionLimit, BiFunction<String, Character, Double> substitutionFunction) {
         NodeMappingAlgorithm algorithm = null;
-        //TODO: loose the switch
         switch (node.getType()) {
             case LEAF:
                 algorithm = new LeafMappingAlgorithm(string, node, treeDeletionLimit,
--- src/structures/NodeType.java
+++ src/structures/NodeType.java
@@ -1,7 +1,21 @@
 package structures;
 
+import NodeMappingAlgorithms.LeafMappingAlgorithm;
+import NodeMappingAlgorithms.PNodeMappingAlgorithm;
+import NodeMappingAlgorithms.QNodeMappingAlgorithm;
+
 public enum NodeType {
-    LEAF,
-    P,
-    Q
+    LEAF (LeafMappingAlgorithm.class),
+    P (PNodeMappingAlgorithm.class),
+    Q (QNodeMappingAlgorithm.class);
+
+    private final Class mappingAlgorithm;
+
+    NodeType(Class mappingAlgorithm) {
+        this.mappingAlgorithm = mappingAlgorithm;
+    }
+
+    public Class<?> getMappingAlgorithm() {
+        return mappingAlgorithm;
+    }
 }
