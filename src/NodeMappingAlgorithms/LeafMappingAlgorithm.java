024fdc4f72d7d5f43e3747d817c3d6dbb97ea240
zimgalia <zimgalia@gmail.com>
2019-12-30 11:11:48 +0200

    Fix Leaf mapping algorithm to return one mapping per deletion combination and refactor

--- src/NodeMappingAlgorithms/LeafMappingAlgorithm.java
+++ src/NodeMappingAlgorithms/LeafMappingAlgorithm.java
@@ -3,7 +3,7 @@ package NodeMappingAlgorithms;
 import structures.Mapping;
 import structures.Node;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 
@@ -15,27 +15,18 @@ public class LeafMappingAlgorithm extends NodeMappingAlgorithm {
 
     @Override
     public void runAlgorithm() {
-        List<Mapping> mappings;
-        Mapping mapping;
         Double score;
-        for (int i = 0; i <= string.length(); i++) {
-            mappings = new LinkedList<>();
-            for (int dT = 0; dT <= treeDeletionLimit; dT++) {
-                for (int dS = 0; dS <= stringDeletionLimit; dS++) {
-                    score = Double.NEGATIVE_INFINITY;
-                    if (i == 0 & dS == 0) {
-                        mapping = new Mapping(node, i, dS, dT, score);
-                        mappings.add(mapping);
-                    }
-                }
-            }
-            if (i == 0)
-                score = 0.0;
-            else
-                score = substitutionFunction.apply(node.getCog(),string.charAt(i-1));
-            mapping = new Mapping(node, i, i, 0, 0, score);
-            mappings.add(mapping);
-            resultMappingsByEndPoints.put(i, mappings);
+        addMappingByEndPoint(0, 0.0);
+        for (int i = 1; i <= string.length(); i++) {
+            score = substitutionFunction.apply(node.getCog(),string.charAt(i-1));
+            addMappingByEndPoint(i, score);
         }
+    }
+
+    private void addMappingByEndPoint(int endPoint, Double score) {
+        Mapping mapping = new Mapping(node, endPoint, endPoint, 0, 0, score);
+        List<Mapping> mappingList = new ArrayList<>();
+        mappingList.add(mapping);
+        resultMappingsByEndPoints.put(endPoint, mappingList);
     }
 }
